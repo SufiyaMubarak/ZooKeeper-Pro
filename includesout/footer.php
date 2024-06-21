0dad93775ed673e9978aa722d9ea9f735c44f987
SufiyaMubarak <159440755+SufiyaMubarak@users.noreply.github.com>
2024-06-20 23:06:59 -0700

    Add files via upload

---
+++ includesout/footer.php
@@ -0,0 +1,7 @@
+<div class="footer-section">
+				<div class="container">
+					<div class="footer-top">
+						<p>&copy; <?php echo date('Y')?>Sufiya's ZooKeeper Pro</p>
+					</div>
+				</div>
+			</div>
